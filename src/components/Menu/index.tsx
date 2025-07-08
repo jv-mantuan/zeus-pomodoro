c0b049e4eaa75d26564f9ac297b13a9e39bab3af
jv-mantuan <jvoliveira3011@gmail.com>
2025-07-07 22:02:24 -0300

    Add page footer and working theme toggle

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,37 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault(); // Prevent the default anchor behavior
+
+    setTheme(prevTheme => prevTheme === 'dark' ? 'light' : 'dark'); 
+  }
+
+  useEffect(() => {document.documentElement.setAttribute('data-theme', theme);}, [theme]);
+
   return (
     <>
       <nav className={styles.menu}>
-        <a className={styles.menuLink} href='#'>
+        <a className={styles.menuLink} href='#' aria-label='Ir para a Home' title='Ir para a Home'>
           <HouseIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a className={styles.menuLink} href='#' aria-label='Ver histórico' title='Ver histórico'>
           <HistoryIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a className={styles.menuLink} href='#' aria-label='Configurações' title='Configurações'>
           <SettingsIcon />
         </a>
 
-        <a className={styles.menuLink} href='#'>
+        <a className={styles.menuLink} href='#' aria-label='Mudar tema' title='Mudar tema' onClick={handleThemeChange}>
           <SunIcon />
         </a>
       </nav>
